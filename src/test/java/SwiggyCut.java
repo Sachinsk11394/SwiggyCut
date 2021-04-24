f2bd8dbb32529ab2da8c000780f8833a23457555
Sachin <sachin.sampathkumar@care.com>
2021-04-25 01:28:34 +0530

    Added necessary comments

--- src/test/java/SwiggyCut.java
+++ src/test/java/SwiggyCut.java
@@ -16,33 +16,46 @@ public class SwiggyCut {
 
     @Test
     public void SwiggyCutTest() throws InterruptedException {
+        /**
+         * Run this on terminal
+         * Google\ Chrome --remote-debugging-port=9222 --user-data-dir="~//Users/user_name/Library/Application Support/Google/Chrome"
+         */
         options.addArguments("user-data-dir=/Users/sachin.sampathkumar/Library/Application Support/Google/Chrome/");
         options.setExperimentalOption("debuggerAddress", "127.0.0.1:9222");
         WebDriver driver = new ChromeDriver(options);
-        Actions actions = new Actions(driver);
+
+        // Load swiggy orders page directly. We need to have prior chrome setup to run on a pre-defined profile
         driver.get("https://www.swiggy.com/my-account/orders");
+
+        //Todo: Use a better way to make browser wait for feedback element to load
         Thread.sleep(2000);
 
-        JavascriptExecutor js = (JavascriptExecutor) driver;;
+        //Remove feedback popup
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("return document.getElementById('nps-nudge').remove();");
 
-        WebElement more = driver.findElement (By.className ("_2pWZz"));
-        Integer numberOfOrders = 0;
+        // More order element. Class name might change with time
+        WebElement more = driver.findElement(By.className("_2pWZz"));
 
-        while (numberOfOrders != driver.findElements(By.className("_3Hghg")).size()) {
-            numberOfOrders = driver.findElements(By.className("_3Hghg")).size();
+        int numberOfOrders = -1;
+        int lastCountOfOrders = 0;
+        while (numberOfOrders != lastCountOfOrders) {
+            numberOfOrders = lastCountOfOrders;
             System.out.println("Order number " + numberOfOrders + " and counting...");
-            JavascriptExecutor executor = (JavascriptExecutor)driver;
+            JavascriptExecutor executor = (JavascriptExecutor) driver;
             executor.executeScript("arguments[0].click();", more);
             Thread.sleep(400);
+            lastCountOfOrders = driver.findElements(By.className("_3Hghg")).size();
         }
 
+        // Calculate total cost
+        int totalCost = 0;
         List<WebElement> costs = driver.findElements(By.className("_3Hghg"));
-        Integer totalCost = 0;
         for (WebElement cost : costs) {
             totalCost = totalCost + Integer.parseInt(cost.getText());
         }
 
+        // Display first order on swiggy
         List<WebElement> orderTimes = driver.findElements(By.className("_2uT6l"));
         String firstOrderTime = orderTimes.get(orderTimes.size() - 1).getText().split("\\|")[1];
 
